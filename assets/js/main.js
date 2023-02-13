daf81fd3012ae62c8baee83f96c2829c894f4857
Nelsonrsl.com <nelsonribera1@gmail.com>
2023-02-13 09:20:13 -0400

    proyecto inicial

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var toggle = document.getElementById("theme-switch");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: light)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php $root = $_SERVER['DOCUMENT_ROOT'];?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logistica Express</title>
+    <link rel="stylesheet" href="<?php $root?>/assets/css/style.css">
+</head>
+<body>
+    <button id="theme-switch">Mode</button>
+    
+    <script src="<?php $root?>/assets/js/main.js"></script>
+</body>
+</html>
