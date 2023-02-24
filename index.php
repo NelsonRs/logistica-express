cf18a014d9f466653d4d8c00da69c01a0146a7db
NelsonRs <nelsonribera1@gmail.com>
2023-02-24 01:05:08 -0400

    add hero

--- index.php
+++ index.php
@@ -9,7 +9,36 @@
     <link rel="stylesheet" href="<?php $root?>/assets/css/style.css">
 </head>
 <body>
-    <button id="theme-switch">Mode</button>
+    <main>
+        <nav>
+            <div class="nav-it-logo">
+                <a href="?">Logo</a>
+            </div>
+            <div class="nav-it-link">
+                <a href="?">Inicio</a>
+                <a href="?">Cobertura</a>
+                <a href="?">Servicio</a>
+                <a href="?">Quienes Somos</a>
+                <a href="?">Contacto</a>
+            </div>
+            <div class="nav-it-access">
+                <a href="?">Trackea tu pedido</a>
+            </div>
+        </nav>
+
+        <a id="theme-switch"></a>
+
+        <div class="banner-hero">
+            <img src="assets/img/banner-hero.jpg">
+            <p>Logística Express</p>
+            <p>Haz crecer tu negocio</p>
+        </div>
+
+    </main>
+    
+
+    
+    
     
     <script src="<?php $root?>/assets/js/main.js"></script>
 </body>
